18b0c81806f20992d37c2f9c19b3e88a0f8f6594
Robert Syme <rob.syme@gmail.com>
2022-12-15 21:30:20 +0000

    Initial commit

---
+++ src/nextflow/README.md
@@ -0,0 +1,18 @@
+
+# Nextflow
+
+A feature to install Nextflow to allow quick and easy pipeline development in Github Codespaces
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/robsyme/features/nextflow:1": {
+        "version": "latest"
+    }
+}
+```
+
+## Options
+
+No options required.
---
+++ src/nextflow/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Maintainer: Rob Syme
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+NEXTFLOW_DIR=/opt/nextflow
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+if ! cat /etc/group | grep -e "^nextflow:" > /dev/null 2>&1; then
+    groupadd -r nextflow
+fi
+
+usermod -a -G nextflow "${USERNAME}"
+
+echo "Installing Nextflow..."
+
+mkdir -p $NEXTFLOW_DIR/bin
+cd $NEXTFLOW_DIR/bin
+curl -s https://get.nextflow.io | bash
+
+chown -R "${USERNAME}:nextflow" "${NEXTFLOW_DIR}"
+chmod -R g+r+w "${NEXTFLOW_DIR}"
+
+find "${NEXTFLOW_DIR}" -type d -print0 | xargs -n 1 -0 chmod g+s
+
+echo "Done!"
