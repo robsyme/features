9fe0eac0f7528b471d89c6ce86cc2750fb4ce00a
Josh Spicer <josh@joshspicer.com>
2023-01-17 09:34:44 -0800

    Add info on injected environment variables  (#16)
    * info on new env variable
    
    * checkpoint for expanding with remoteUser
    
    * Update install.sh
    
    * Update my_favorite_color_is_green.sh
    
    * execute in correct shell
    
    * best test string

--- test/nf-test/test.sh
+++ test/nf-test/test.sh
@@ -15,7 +15,8 @@
 # will fall back to the default value in the feature's 'devcontainer-feature.json'
 # For the 'color' feature, that means the default favorite color is 'red'.
 # 
-# This test can be run with the following command (from the root of this repo)
+# This test can be run with the following command:
+#
 #    devcontainer features test \ 
 #               --features color \
 #               --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
@@ -23,6 +24,7 @@
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
+# Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
 # Feature-specific tests
